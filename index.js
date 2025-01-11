84c20ddd548cf572f7b6622fd12a66f933f2c9e6
Nick Aliferov <x8core@gmail.com>
2025-01-11 18:41:35 +0200

    update with new concept

--- index.js
+++ index.js
@@ -1,43 +1,52 @@
 import fs from 'node:fs/promises'
 
-let events = []
-let interrupt
+export const reploidFactory = () => {
 
-const interuptOn = () => {
-    interrupt = Promise.withResolvers()
-    return interrupt.promise
-}
-const interuptOff = () => {
-    interrupt.resolve('interuptOff')
-    interrupt = null
-}
+    const reploid = {
+        events: [],
+        eventHandler: null,
+        inputInterval: null,
+        
+        maxEventsPerSecond: 50,
+        isWorking: false,
+        interruptor: null,
 
-const gen = async function*() {
-    while (true) {
-        for (let i = 0; i < events.length; i++) {
-            console.log('event promise')
-            yield await events[i]
-        }
-        events = []
-        events.push(interuptOn())
+        pause: () => {
+            this.interruptor = Promise.withResolvers()
+            return this.interruptor.promise
+        },
+        play() {
+            this.interruptor.resolve('interruptor off')
+            this.interruptor = null
+        },
+        async start() {
+            //generate this with promise and put to event
+
+            // setInterval(async () => {
+            //     let input
+            //     try {
+            //         const inputJson = await fs.readFile(this.inputPath, 'utf-8')
+            //         input = JSON.parse(configJson)
+            //     } catch (e) {
+            //         console.error('error in config proccing', e)
+            //     }
+            // }, 2000)
+
+            while (true) {
+                for (let i = 0; i < events.length; i++) {
+                    console.log('event promise')
+                    this.eventHandler(await events[i])
+                }
+                events = []
+                events.push(this.pause())
+            }
+        },
+        setInputPath() {
+        },
+        setEventHandler(eventHandler) {
+            this.eventHandler = eventHandler
+        },
     }
-}
 
-setInterval(async () => {
-    let config
-    try {
-        const configJson = await fs.readFile('./config.json', 'utf-8')
-        config = JSON.parse(configJson)
-    } catch (e) {
-        console.error('error in config proccing', e)
-    }
-}, 2000)
-
-for await (const val of gen()) {
-    console.log(val)
-}
-
-
-const reploid = () => {
-
+    return reploid
 }
